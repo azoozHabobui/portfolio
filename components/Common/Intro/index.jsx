2003a290fbfa7324d6149fd80e62446e002c0192
azoozHabobui <aziz789haboubi@gmail.com>
2025-07-20 18:55:31 +0100

    modfied image

--- components/Common/Intro/index.jsx
+++ components/Common/Intro/index.jsx
@@ -8,7 +8,7 @@ import Tools from './Tools';
 import Skills from './Skills';
 import Image from 'next/image';
 import { NAME, DESIGNATION, SOCIAL_LINKS } from '../../../constants/constants';
-import Osama from '../../../public/images/Haboubi.jpg';
+import Osama from '../../../public/images/Haboubi.png';
 
 const Intro = () => {
   return (
