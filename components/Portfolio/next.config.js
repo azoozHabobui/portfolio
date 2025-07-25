347bb220c89fd4d240b2310dbc7004650e9c847c
azoozHabobui <aziz789haboubi@gmail.com>
2025-07-25 19:18:31 +0100

    taswira

--- components/Portfolio/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    unoptimized: true, // disable image optimization in local
-  },
-}
-
-module.exports = nextConfig;
--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true, // disable image optimization in local
   },
 }
+
+module.exports = nextConfig;
